58ffc6b9f2aeaff54faae617d4a8788eebf2ea93
Lucasboina <lucasmmantelli@gmail.com>
2024-12-09 14:38:50 -0300

    .

--- Aula6/php/site/database/banco.sql
+++ Aula6/php/site/database/banco.sql
@@ -36,7 +36,6 @@ INSERT INTO `categoria` (`id`, `nome`) VALUES
 	(19, 'asdasdsada'),
 	(20, 'dadad'),
 	(21, ''),
-	(22, ''),
 	(23, 'lucas molozzi asdasdasdad'),
 	(24, 'lucasasdasdamolozzi asdasdasdad'),
 	(25, '333'),
@@ -48,14 +47,16 @@ CREATE TABLE IF NOT EXISTS `post` (
   `texto` text NOT NULL,
   `data_publicacao` datetime NOT NULL,
   `status` tinyint(1) NOT NULL,
-  `título` varchar(150) NOT NULL,
+  `titulo` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `categoria_id` bigint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `FK_post_categoria` (`categoria_id`),
   CONSTRAINT `FK_post_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela db_pweb1_2024_2_blog.post: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela db_pweb1_2024_2_blog.post: ~1 rows (aproximadamente)
+INSERT INTO `post` (`id`, `texto`, `data_publicacao`, `status`, `titulo`, `categoria_id`) VALUES
+	(2, '123', '2024-12-02 16:22:00', 0, 'qeqeqweqe', 20);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
